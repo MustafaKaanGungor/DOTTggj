95abe5f46f46719e23a51fa4dfdc325f2468a069
YavuzKibiroglu <24181616021@gazi.edu.tr>
2025-01-25 00:14:25 +0300

    Bubble System Base
    Made the base of bubble system

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public static Player Instance {get; private set;}
+    #region Bubble System References
+    public static  float bubbleAmount;
+    [Header("BubbleAmount References")]
+    [SerializeField] private float bubbleAmountAtStart;
+    [SerializeField] private float bubbleSpendPerAttack;
+    [SerializeField] private float bubbleSpendPerSecond;
+    private float bubbleTimer;
+    #endregion
     private void Awake() {
         Instance = this;
     }
     void Start()
     {
+        bubbleAmount = bubbleAmountAtStart;
         GameInput.Instance.OnAttack += PlayerOnAttack;
         GameInput.Instance.OnDash += PlayerOnDash;
     }
@@ -23,16 +32,29 @@ public class Player : MonoBehaviour
     {
         //oyuncu saldırıyor
         Debug.Log("heyo");
+        bubbleAmount -= bubbleSpendPerAttack;
     }
 
     void Update()
     {
         Movement();
+        DecreaseBubblePerSecond();
+        Debug.Log(bubbleAmount);
         
     }
 
     private void Movement() {
-        Debug.Log(GameInput.Instance.GetMovementVector());
+       
         //hareket
+    }
+    //Decrease the bubble amount per second
+    private void DecreaseBubblePerSecond()
+    {
+        bubbleTimer += Time.deltaTime;
+        if(bubbleTimer >= 1)
+        {
+            bubbleAmount -= bubbleSpendPerSecond;   
+            bubbleTimer = 0;
+        }
     }
 }

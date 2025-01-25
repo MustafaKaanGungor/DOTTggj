16ee711a33067d894c8ab244380d9059a411703b
Mehmet Ali <mehmetaliozek761@gmail.com>
2025-01-25 20:58:17 +0300

    Dash barı animasyonu eklendi

--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,17 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private Image dashBar;
     [SerializeField] private Image healthSlider;
 
-    private void Start() {
+    private void Start()
+    {
         Player.Instance.OnPlayerHealthUpdated += PlayerOnPlayerHealthUpdated;
+        Player.Instance.OnPlayerDashUpdated += PLayerOnPlayerDashUpdated; 
         healthSlider.fillAmount = 1f;
+    }
+
+    private void PLayerOnPlayerDashUpdated(object sender, EventArgs e)
+    {
+        float[] dashParametres = (float[])sender;
+        StartCoroutine(DashCoolDownResetCoroutine(dashParametres[0], dashParametres[1]));
     }
 
     private void PlayerOnPlayerHealthUpdated(object sender, EventArgs e)
@@ -17,8 +26,31 @@ public class PlayerUI : MonoBehaviour
         healthSlider.fillAmount = Player.Instance.GetBubbleAirPercentage();
     }
 
-    public void UpdateVisuals() {
+    public void UpdateVisuals()
+    {
         //dashBar.fillAmount = Player.Instance.
         //healthSlider.value = Player.Instance.GetBubbleAirCurrent();
+    }
+
+    IEnumerator DashCoolDownResetCoroutine(float dashingTime, float dashingCooldown)
+    {
+        float elapsed = 0f;
+        while (elapsed < dashingTime)
+        {
+            dashBar.fillAmount = Mathf.Lerp(1f, 0f, elapsed / dashingTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        dashBar.fillAmount = 0f;
+
+        elapsed = 0f;
+        while (elapsed < dashingTime)
+        {
+            dashBar.fillAmount = Mathf.Lerp(0f, 1f, elapsed / dashingCooldown);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        dashBar.fillAmount = 1f;
     }
 }

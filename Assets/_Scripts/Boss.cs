12fb14822d42d5ecc21d716b5a685e409ae57043
Mehmet Ali <mehmetaliozek761@gmail.com>
2025-01-25 13:06:08 +0300

    Boss için 2 saldırı tasarlandı

--- Assets/_Scripts/Boss.cs
+++ Assets/_Scripts/Boss.cs
@@ -1,23 +1,100 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
     [SerializeField] private LayerMask playerMask;
     [SerializeField] private Transform tentacle;
-    [SerializeField] private  float attackDelay = 1.5f;
+    [SerializeField] private float attackDelay = 1.5f;
     [SerializeField] private float attackWeight = 3f;
     [SerializeField] private int attackDamage;
     [SerializeField] private GameObject attackEffect;
 
-    
+
+    [SerializeField] private GameObject tentacleParent;
+    [SerializeField] private GameObject tentaclePrefab;
+    [SerializeField] private GameObject tentacleLongParent;
+    [SerializeField] private GameObject[] tentacleLong;
+    [SerializeField] private Collider2D spawnAreaCollider;
+
+    private List<GameObject> tentaclePool = new List<GameObject>();
+
     void Start()
     {
-        //
+        TentaclePooling(20);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Test etmek için eklenmiþtir
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BottomUpTentacleAttack();
+            RotateTentacleAttack();
+        }
     }
+
+    public void TentaclePooling(int tentacleCount)
+    {
+        int spawnedTenctacleCount = 0;
+        while (spawnedTenctacleCount != tentacleCount)
+        {
+            var tentacle = Instantiate(tentaclePrefab, transform.position, Quaternion.identity);
+            tentacle.transform.parent = tentacleParent.transform;
+            tentacle.SetActive(false);
+            tentaclePool.Add(tentacle);
+            spawnedTenctacleCount++;
+        }
+    }
+
+    public List<Vector2> GenerateRandomPositions(int count, float minDistance)
+    {
+        List<Vector2> positions = new List<Vector2>();
+        int maxAttempts = 1000;
+
+        while (positions.Count < count)
+        {
+            if (maxAttempts-- <= 0)
+            {
+                Debug.LogError("Maksimum deneme ulaþýldý");
+                break;
+            }
+
+            Vector2 pos = new Vector2(Random.Range(-12, 12), Random.Range(-8, 8));
+            Vector2 candidate = spawnAreaCollider.ClosestPoint(pos);
+
+            if (pos != candidate) continue;
+            if (candidate == tentacleParent.GetComponent<Collider2D>().ClosestPoint(candidate)) continue;
+            if (positions.Exists(pos => Vector2.Distance(pos, candidate) < minDistance)) continue;
+
+            positions.Add(candidate);
+        }
+
+        return positions;
+    }
+
+    private void BottomUpTentacleAttack()
+    {
+        var randomPositions = GenerateRandomPositions(20, 2.5f);
+
+        for (int i = 0; i < randomPositions.Count; i++)
+        {
+            tentaclePool[i].transform.position = randomPositions[i];
+        }
+
+        foreach (var item in tentaclePool)
+        {
+            item.SetActive(true);
+        }
+    }
+
+    private void RotateTentacleAttack()
+    {
+        foreach (var item in tentacleLong)
+        {
+            item.SetActive(true);
+        }
+    }
+
 }
---
+++ Assets/_Scripts/Tentacles.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Tentacles : MonoBehaviour
+{
+    [SerializeField] private GameObject tentacle;
+
+    [SerializeField] private TentacleType tentacleType;
+
+    private readonly float life = 0.5f;
+
+    private bool isAttacking = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && isAttacking)
+        {
+            // can azalt
+        }
+    }
+
+    private void OnEnable()
+    {
+        switch (tentacleType)
+        {
+            case TentacleType.BottomUp:
+                StartCoroutine(TentacleBottomUpAttackCoroutine());
+                break;
+            case TentacleType.Rotate:
+                StartCoroutine(TentacleRotateAttackCoroutine());
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator TentacleBottomUpAttackCoroutine()
+    {
+        yield return new WaitForSeconds(life);
+        tentacle.SetActive(true);
+        isAttacking = true;
+        yield return new WaitForSeconds(life);
+        isAttacking = false;
+        tentacle.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator TentacleRotateAttackCoroutine()
+    {
+        isAttacking = true;
+        float duration = life / 2;
+        Vector3 startScale = tentacle.transform.localScale;
+        Vector3 targetScale = new Vector3(1f, startScale.y, startScale.z);
+        Quaternion startRotation = tentacle.transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, tentacle.transform.eulerAngles.z - 90);
+
+        yield return LerpTransform(duration, t =>
+        {
+            tentacle.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+        });
+
+        yield return new WaitForSeconds(duration);
+
+        yield return LerpTransform(duration, t =>
+        {
+            tentacle.transform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+        });
+
+        yield return new WaitForSeconds(duration);
+
+        yield return LerpTransform(duration, t =>
+        {
+            tentacle.transform.localScale = Vector3.Lerp(targetScale, startScale, t);
+        });
+
+        isAttacking = false;
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator LerpTransform(float duration, Action<float> updateAction)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            updateAction(Mathf.Clamp01(elapsedTime / duration));
+            yield return null;
+        }
+        updateAction(1f);
+    }
+}
+
+public enum TentacleType
+{
+    BottomUp,
+    Rotate
+}

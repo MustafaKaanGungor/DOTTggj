689cafd370e4c43fbb9e969938db29d1e52631ab
Abdülhamid Uysal <hamituysal25@gmail.com>
2025-01-25 20:01:10 +0300

    bubbleSpawner

--- Assets/_Scripts/Bubble.cs
+++ Assets/_Scripts/Bubble.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 public class Bubble : MonoBehaviour
 {
     [SerializeField] private float bubbleValue;
-
+    [SerializeField] private int bubbleTTL = 5;
+    private float currentTime = 0;
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.tag == "Player")
         {
             Player.Instance.HealBubbleAir(bubbleValue);
             Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        DestroyBubble();
+    }
+    void DestroyBubble()
+    {
+       currentTime += Time.deltaTime;
+        if (currentTime >= bubbleTTL)
+        {
+            Destroy(gameObject);
+            currentTime = 0f;
         }
     }
 }

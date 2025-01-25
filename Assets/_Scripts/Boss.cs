7fbbb68b47124311eacb42c2180e6b3b199c4dbc
Abdülhamid Uysal <hamituysal25@gmail.com>
2025-01-25 10:25:18 +0300

    BossLineAttack

--- Assets/_Scripts/Boss.cs
+++ Assets/_Scripts/Boss.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
@@ -5,19 +6,39 @@ public class Boss : MonoBehaviour
     [SerializeField] private LayerMask playerMask;
     [SerializeField] private Transform tentacle;
     [SerializeField] private  float attackDelay = 1.5f;
-    [SerializeField] private float attackWeight = 3f;
+    [SerializeField] private float attackWidht = 1f;
+    [SerializeField] private float attackHeight = 4f;
     [SerializeField] private int attackDamage;
     [SerializeField] private GameObject attackEffect;
 
     
     void Start()
     {
-        //
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+    }
+    private void LineAttack(Vector2 targetPos)
+    {
+        StartCoroutine(TentacleLineAttack(targetPos));
+    }
+    private IEnumerator TentacleLineAttack(Vector2 targetPos)
+    {
+
+        yield return new WaitForSeconds(attackDelay);
+        Vector2 boxSize = new Vector2(attackWidht, attackHeight);
+        Collider2D[] hitObjects = Physics2D.OverlapBoxAll(tentacle.transform.position, boxSize, 0f, playerMask);
+        foreach (Collider2D hitObject in hitObjects)
+        {
+            if(hitObject.CompareTag("Player")) 
+            {
+                //player take damage metodu
+                Debug.Log("Attacked player");
+            }
+        }
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(tentacle.position, new Vector2(attackWidht, attackHeight));
     }
 }

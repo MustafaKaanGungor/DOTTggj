27008654043afc4bf385de7271d8b7d0b9865229
Mehmet Ali <mehmetaliozek761@gmail.com>
2025-01-26 10:30:48 +0300

    yeni görseller eklendi

--- Assets/_Scripts/PlayerAim.cs
+++ Assets/_Scripts/PlayerAim.cs
@@ -16,6 +16,10 @@ public class PlayerAim : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.right = (mousePos - (Vector2)transform.position).normalized;
         if(GameInput.Instance.IsAttacking()) {
+            if (fireEffect.GetComponent<ParticleSystem>().isStopped)
+            {
+                fireEffect.GetComponent<ParticleSystem>().Play();
+            }
             selfDamageTimer += Time.deltaTime;
             if(selfDamageTimer >= 1) {
                 Player.Instance.DamageBubbleAir(2);
@@ -27,7 +31,10 @@ public class PlayerAim : MonoBehaviour
                     hit.collider.GetComponent<Boss>().DamageHealth(damage);
                 }
             }
-            
+        }
+        else
+        {
+            fireEffect.GetComponent<ParticleSystem>().Stop();
         }
     }
 }
